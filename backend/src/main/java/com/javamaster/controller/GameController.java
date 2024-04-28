97905cb8c6a65f0aa50ebf8977529827c9bcb3b7
Katsiaryna Viarenich <katsiaryna.viarenich@gmail.com>
2024-04-28 20:01:55 +0200

    Update GameController.java

--- backend/src/main/java/com/javamaster/controller/GameController.java
+++ backend/src/main/java/com/javamaster/controller/GameController.java
@@ -23,28 +23,28 @@ public class GameController {
     private final GameService gameService;
     private final SimpMessagingTemplate simpMessagingTemplate;
 
-    @CrossOrigin(origins = "http://100.26.104.115:8081")
+    @CrossOrigin(origins = "http://54.225.45.125:8081")
     @PostMapping("/start")
     public ResponseEntity<Game> start(@RequestBody Player player) {
         log.info("start game request: {}", player);
         return ResponseEntity.ok(gameService.createGame(player));
     }
 
-    @CrossOrigin(origins = "http://100.26.104.115:8081")
+    @CrossOrigin(origins = "http://54.225.45.125:8081")
     @PostMapping("/connect")
     public ResponseEntity<Game> connect(@RequestBody ConnectRequest request) throws InvalidParamException, InvalidGameException {
         log.info("connect request: {}", request);
         return ResponseEntity.ok(gameService.connectToGame(request.getPlayer(), request.getGameId()));
     }
 
-    @CrossOrigin(origins = "http://100.26.104.115:8081")
+    @CrossOrigin(origins = "http://54.225.45.125:8081")
     @PostMapping("/connect/random")
     public ResponseEntity<Game> connectRandom(@RequestBody Player player) throws NotFoundException {
         log.info("connect random {}", player);
         return ResponseEntity.ok(gameService.connectToRandomGame(player));
     }
 
-    @CrossOrigin(origins = "http://100.26.104.115:8081")
+    @CrossOrigin(origins = "http://54.225.45.125:8081")
     @PostMapping("/gameplay")
     public ResponseEntity<Game> gamePlay(@RequestBody GamePlay request) throws NotFoundException, InvalidGameException {
         log.info("gameplay: {}", request);
