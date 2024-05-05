038fed37155288b02825000a18cea19b7d172b7f
Katsiaryna Viarenich <katsiaryna.viarenich@gmail.com>
2024-05-05 19:00:42 +0200

    Add files via upload

---
+++ backend/src/main/java/com/javamaster/config/WebsocketConfiguration.java
@@ -0,0 +1,27 @@
+package com.javamaster.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebsocketConfiguration implements WebSocketMessageBrokerConfigurer {
+    @Value("${ip_address}")
+    private String ipAddress;
+    
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/gameplay").setAllowedOriginPatterns("http://" + ipAddress + ":8081").withSockJS();
+
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.setApplicationDestinationPrefixes("/app").enableSimpleBroker("/topic");
+    }
+}
+
