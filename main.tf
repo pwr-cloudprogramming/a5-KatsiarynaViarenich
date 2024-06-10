755e74c92f8a22345aa82adc89efd13aef7f8b42
Katsiaryna Viarenich <katsiaryna.viarenich@gmail.com>
2024-06-10 14:50:47 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,79 +9,69 @@ terraform {
 }
 
 provider "aws" {
-    region = "us-east-1"
+  region = "us-east-1"
 }
 
 resource "aws_instance" "app_server" {
-    ami = "ami-0d7a109bf30624c99"
-    instance_type = "t2.micro"
-    tags = {
-        Name = "MyServerKVA5"
-        Terraform = "true"
-    }
+  ami           = "ami-0d7a109bf30624c99"
+  instance_type = "t2.micro"
+  tags = {
+    Name      = "MyServerKVA5"
+    Terraform = "true"
+  }
 }
 resource "aws_security_group" "allow_ssh_http" {
-    name = "allow_ssh_http"
-    description = "Allow SSH and HTTP inbound traffic and all outbound traffic"
-    vpc_id = module.my_vpc.vpc_id
-    tags = {
-        Name = "allow-ssh-http"
-        }
+  name        = "allow_ssh_http"
+  description = "Allow SSH and HTTP inbound traffic and all outbound traffic"
+  vpc_id      = module.my_vpc.vpc_id
+  tags = {
+    Name = "allow-ssh-http"
+  }
 }
 
 module "my_vpc" {
-    source = "terraform-aws-modules/vpc/aws"
-    name = "my-vpc-KVA5"
-    cidr = "10.0.0.0/16"
-    azs = ["us-east-1b"]
-    public_subnets = ["10.0.101.0/24"]
-    tags = {
-        Terraform = "true"
-        Environment = "dev"
-    }
+  source         = "terraform-aws-modules/vpc/aws"
+  name           = "my-vpc-KVA5"
+  cidr           = "10.0.0.0/16"
+  azs            = ["us-east-1b"]
+  public_subnets = ["10.0.101.0/24"]
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
 }
 
 resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
-    security_group_id = aws_security_group.allow_ssh_http.id
-    cidr_ipv4 = "0.0.0.0/0"
-    ip_protocol = "-1" # all ports
+  security_group_id = aws_security_group.allow_ssh_http.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" # all ports
 }
 
 resource "aws_vpc_security_group_ingress_rule" "allow_http" {
-    security_group_id = aws_security_group.allow_ssh_http.id
-    cidr_ipv4 = "0.0.0.0/0"
-    ip_protocol = "tcp"
-    from_port = 8080
-    to_port = 8081
+  security_group_id = aws_security_group.allow_ssh_http.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "tcp"
+  from_port         = 8080
+  to_port           = 8081
 }
 
 resource "aws_vpc_security_group_ingress_rule" "allow_ssh" {
-    security_group_id = aws_security_group.allow_ssh_http.id
-    cidr_ipv4 = "0.0.0.0/0"
-    ip_protocol = "tcp"
-    from_port = 22
-    to_port = 22
+  security_group_id = aws_security_group.allow_ssh_http.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "tcp"
+  from_port         = 22
+  to_port           = 22
 }
 
 resource "aws_instance" "tf-web-server" {
   ami                         = "ami-080e1f13689e07408"
   instance_type               = "t2.micro"
   key_name                    = "vockey"
-  subnet_id = module.my_vpc.public_subnets[0]
+  subnet_id                   = module.my_vpc.public_subnets[0]
   vpc_security_group_ids      = [aws_security_group.allow_ssh_http.id]
   associate_public_ip_address = true
-  user_data = <<-EOF
-              #!/bin/bash
-              sudo apt update
-              sudo apt install -y docker.io
-              sudo systemctl start docker
-              sudo systemctl enable docker
-              sudo usermod -aG docker ubuntu
-              sudo docker pull backend_a5:latest
-              sudo docker pull frontend_a5:latest
-              sudo docker run -d -p 8080:8080 backend_a5:latest
-              sudo docker run -d -p 8081:3000 frontend_a5:latest
-              EOF
+  user_data                   = file("run.sh")
+  user_data_replace_on_change = true
   tags = {
     Name = "A5"
   }
